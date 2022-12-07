86d74edc870c2fa84e79df9f388162a726ca45c8
leeyounggyu <baayoo93@naver.com>
2022-12-08 04:04:22 +0900

    Refactor: 메뉴 dropdown 리팩토링 작업

--- src/components/header/HeaderBar/HeaderBar.tsx
+++ src/components/header/HeaderBar/HeaderBar.tsx
@@ -12,6 +12,11 @@ const HeaderBar = () => {
 
   const handleModalOpen = () => openModal();
 
+  const menus = [
+    { title: "새 글 작성", url: "/write" },
+    { title: "내 블로그 가기", url: `/${session?.user.email}` },
+  ];
+
   return (
     <>
       <LoginModal isActive={modalOpen} handleClose={closeModal} />
@@ -32,7 +37,16 @@ const HeaderBar = () => {
             <IconButton width="30px" height="30px" position="-10px -10px" />
 
             {session ? (
-              <MenuDropdown />
+              <MenuDropdown
+                items={menus}
+                trigger={
+                  <IconButton
+                    width="30px"
+                    height="30px"
+                    position="-10px -110px"
+                  />
+                }
+              />
             ) : (
               <Button
                 variant="primary"
